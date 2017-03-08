a9cd7ee11fb6cfc685c0cdbadafbda9713fe178e
Mkasym <mradko@luxoft.com>
2017-03-08 23:41:28 +0200

    add self registering service with netflix eureka

--- main.go
+++ main.go
@@ -2,13 +2,53 @@ package main
 
 import (
 	"os"
+
 	"github.com/tttmaximttt/fullfillmentservice/service"
+	"github.com/hudl/fargo"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if len(port) == 0 { port = "3000"
+	// For a real app, you'd bind a user-provided service with eureka // credentials and URL.
+	discovery := fargo.NewConn("http://localhost:8080/eureka/v2")
+
+	port, err := strconv.Atoi((os.Getenv("PORT")))
+	host := os.Getenv("HOST")
+
+	if err != nil {
+		fmt.Errorf("%s", err)
 	}
+
+	if len(string(port)) == 0 {
+		port = 3000
+	} else if (len(host)) == 0 {
+		host = "localhost"
+	}
+
+	appInstance := fargo.Instance{
+		HostName: "i-6543",
+		Port: port,
+		App: "FULLFILLMENT_APP",
+		IPAddr: host,
+		VipAddress: host,
+		SecureVipAddress: host,
+		DataCenterInfo: fargo.DataCenterInfo{ Name: fargo.MyOwn },
+		Status: fargo.UP,
+	}
+
+	addressString := fmt.Sprintf("%s:%v", appInstance.IPAddr, appInstance.Port)
+
 	server := service.NewServer()
-	server.Run(":" + port)
+
+	_, err = discovery.GetApp("FULLFILLMENT_APP")
+
+	if err != nil {
+		discovery.RegisterInstance(&appInstance)
+	}
+
+	if err != nil {
+		fmt.Errorf("%s", err)
+	}
+	server.Run(addressString)
 }
